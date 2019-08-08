a6e7b6bfdf231123330558d47daf79b04da7273e
USER1 <USER1@DOMAIN.local>
2019-08-08 15:39:56 +0300

    a

--- GlobusSatProject/src/SubSystemModules/Maintenance/Maintenance.c
+++ GlobusSatProject/src/SubSystemModules/Maintenance/Maintenance.c
@@ -57,6 +57,7 @@ Boolean IsGroundCommunicationWDTKick()
 int SetGsWdtKickTime(time_unix new_gs_wdt_kick_time)
 {
 
+	//by diana
 	if (new_gs_wdt_kick_time< 86400*2 || new_gs_wdt_kick_time>1*86400)
 		return -3;
 	int err=  FRAM_write(&new_gs_wdt_kick_time, LAST_WAKEUP_TIME_ADDR, LAST_WAKEUP_TIME_SIZE);
@@ -67,6 +68,7 @@ int SetGsWdtKickTime(time_unix new_gs_wdt_kick_time)
 
 time_unix GetGsWdtKickTime()
 {
+	//by diana
 	time_unix no_comm_thresh = 0;
 	int err= FRAM_read(&no_comm_thresh, NO_COMM_WDT_KICK_TIME_ADDR, NO_COMM_WDT_KICK_TIME_SIZE);
 	if (err!=0)
